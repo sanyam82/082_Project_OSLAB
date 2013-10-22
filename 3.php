bcecfef6eba8820d5ba868f5821e7d3244c395f2
sanyam82 <sanyam2082@gmail.com>
2013-10-22 16:21:26 +0530

    Create 3.php

---
+++ 3.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="3.php" accesskey="1" title="">Contact Us</a></li>
+				<li><a href="2.php" accesskey="2" title="">Login</a></li>
+				<li><a href="index.php" accesskey="5" title="">Homepage</a></li>
+			</ul>
+    </div>
+		<div id="logo">
+			<h1><a href="#">Ongoing Events In Campus</a></h1>
+			<span><span style="color:black">Explore</span> be a part of every event</span></div>
+	</div>
+</div>
+<div id="wrapper1">
+	<div id="welcome" class="container">
+		<div class="title">
+			
+		</div>
+		<div class="content">
+        <h3 align="left" style="color:White">Contact Us:</h3><br>
+        <img height="50px" width="50px" align="left" src="12.jpg">
+	    <p align="left"> &nbsp;Sanyam Jain<br>&nbsp;Mob: 9677821491<br>Sanyam02@gmail.com
+        
+        
+		</p>
+          
+            
+            
+	</div>
+</div>
+
+	</div>
+</div>
+
+</body>
+</html>
