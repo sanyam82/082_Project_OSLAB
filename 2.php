3d11f4c079062950a84670aaa9df58f07ce2e79b
sanyam82 <sanyam2082@gmail.com>
2013-10-22 16:25:30 +0530

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="2.php" accesskey="1" title="">Login</a></li>
+				<li><a href="index.php" accesskey="2" title="">homepage</a></li>
+				<li><a href="3.php" accesskey="5" title="">Contact Us</a></li>
+			</ul>
+    </div>
+		<div id="logo">
+			<h1><a href="#">Ongoing Events In Campus</a></h1>
+			<span><span style="color:black">Explore</span> be a part of every event</span></div>
+	</div>
+</div>
+<div id="wrapper1">
+	<div id="welcome" class="container">
+		<div class="title">
+			<h2>LOGIN</h2>
+		</div>
+		<div class="content">
+	    <p><form action="chklogin.php">
+              <table align="center">
+              <tr>
+              <td> User name :</td>
+              <td><input type="text" name="firstname" required></td><br><br>
+               </tr>
+               <tr>
+               <td>Password   :</td>
+               <td><input type="password" name="pwd" required></td>
+               </tr>
+               
+              </table>
+              <input type="submit" value="Login">
+              <a href="4.php"><button type="button">Signup</button></a>
+           </form>
+		</p>
+          
+            
+            
+	</div>
+</div>
+
+	</div>
+</div>
+
+</body>
+</html>
