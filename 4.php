d7905fa611eac3cb7b6407ac2653d254e8e92e26
sanyam82 <sanyam2082@gmail.com>
2013-10-22 16:24:04 +0530

    Create 4.php

---
+++ 4.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="2.php" accesskey="1" title="">Login</a></li>
+				<li><a href="index.php" accesskey="2" title="">homepage</a></li>
+				<li><a href="3.php" accesskey="5" title="">Contact Us</a></li>
+			</ul>
+    </div>
+		<div id="logo">
+			<h1><a href="#">Ongoing Events In Campus</a></h1>
+			<span><span style="color:black">Explore</span> be a part of every event</span></div>
+	</div>
+</div>
+<div id="wrapper1">
+	<div id="welcome" class="container">
+		<div class="title">
+			<h2 align="center">SignUp</h2>
+		</div>
+		<div class="content">
+	    <p> <form action="chksignup.php" style="text-align:center">
+             <table align="center">
+             <tr>
+             <td>First name:</td>
+             <td><input type="text" name="fname" id="fname"></td><br>
+             </tr>
+             <tr>
+             <td>Last name:</td>
+             <td><input type="text" name="lname" id="lname"></td>
+             </tr>
+             <tr>
+             <td>Roll-No:</td>
+             <td><input type="text" name="rollno" id="rollno"></td>
+             </tr>
+             <tr>
+             <td>Department :</td>
+             <td><select id="dept" name="dept"  ><option>MCA</option><option>MBA</option><option>ARCH</option><option>ECE</option><option>CIVIL</option><option>COMPSCI</option><option>META</option><option>PROD</option><option>CHEM</option><option>MECH</option><option>ICE</option><option>EEE</option>
+             </select></td></tr>
+             <tr>
+             <td>Username</td>
+             <td><input type="text" id="user" name="user"></td>
+             </tr>
+             <tr>
+             <td>Password:</td>
+             <td><input type="password" name="pass" id="pass"></td>
+             </tr>
+             <tr><td colspan="2" align="center"><input type="submit" value="SignIn"></td></tr>
+             <br>
+              </table>
+              
+           </form>
+		</p>
+          
+            
+            
+	</div>
+</div>
+
+	</div>
+</div>
+
+</body>
+</html>
