2262739580e64e9c37585ca0cc493667d00f13e1
sanyam82 <sanyam2082@gmail.com>
2013-10-22 16:23:26 +0530

    Update chkrequest.php

--- chkrequest.php
+++ chkrequest.php
@@ -1,46 +1,14 @@
+<?php
+$evename=$_REQUEST["evename"];
+$thename=$_REQUEST["thename"];
+$dept=$_REQUEST["dept"];
+$date_f=$_REQUEST["date_f"];
+$date_t=$_REQUEST["date_t"];
+$poster=$_REQUEST["poster"];
+include("conn.php");
+$query1="insert into requests values ('$evename','$thename','$dept','$date_f','$date_t','$poster')";
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+mysql_query($query1) or die(mysql_error());
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link href="default.css" rel="stylesheet" type="text/css" media="all" />
-<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
-
-
-</head>
-<body>
-<div id="header-wrapper">
-	<div id="header" class="container">
-		<div id="menu">
-			<ul>
-				<li class="current_page_item"><a href="index.php" accesskey="1" title="">Homepage</a></li>
-				<li><a href="2.php" accesskey="2" title="">Login</a></li>
-				<li><a href="3.php" accesskey="5" title="">Contact Us</a></li>
-			</ul>
-    </div>
-		<div id="logo">
-			<h1><a href="#">Ongoing Events In Campus</a></h1>
-			<span><span style="color:black">Explore</span> be a part of every event</span></div>
-	</div>
-</div>
-<div id="wrapper1">
-	<div id="welcome" class="container">
-		<div class="title">
-			<h2>Welcome to our website		</h2>
-		</div>
-		<div class="content">
-	    <p>This Application will tell you about all the ongoing events in the campus no matter of which department,in which you can take part . List of events will be generated on the basis of current date. Due to this Application we are lessen the headache of pasting posters around the campus for various events.</p>
-			<a href="#" class="button">Top</a> </div>
-	</div>
-</div>
-
-	</div>
-</div>
-
-</body>
-</html>
+header('location:5.php');
+?>
